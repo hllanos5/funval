7abae311c74b6269115ca3132c645912d57f73c5
Hans Llanos <hans.llanos@gmail.com>
2024-07-08 11:55:50 -0500

    dia10: sesion por mañana

---
+++ modulo2/dia10/src/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import {userRoutes} from './router/userRoutes.js'
+
+const app = express()
+app.use(express.json())
+
+/*
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+})
+
+app.get('/users', (req, res) => {
+    res.send('Lista de usuarios')
+})
+
+app.post('/users', (req, res) => {
+
+    console.log(req.body);
+    res.json({
+        id: 1,
+        nombre: "Hans Llanos"
+    })
+})
+*/
+app.use(userRoutes)
+
+app.listen(3000, () => {
+    console.log("host: http://localhost:3000")
+})
---
+++ modulo2/dia10/src/router/userRoutes.js
@@ -0,0 +1,101 @@
+import {Router} from 'express' 
+
+export const userRoutes = Router()
+
+const users = [
+    {
+        id: 1,
+        nombre : "Hans Llanos",
+        email: "hans.llanos@gmail.com"
+    },
+    {
+        id: 2,
+        nombre : "Hans Llanos2",
+        email: "hans.llanos2@gmail.com"
+    }
+]
+
+//Listar Usuarios
+userRoutes.get('/users', (req, res) => {
+    res.json(users)
+})
+
+// Obtener Usuarios
+userRoutes.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = users.findIndex(users => user.id === id);
+
+    if (index<0) {
+        res.json({
+            error: "Usuario no encontrado"
+        })
+        return
+    }
+    res.json(users[index]);
+})
+
+//Agregar Usuario
+userRoutes.post('/users', (req, res) => {
+    const body = req.body
+    
+    if (!body.nombre){
+        res.json({
+            error: "nombre is required"
+        })
+        return;
+    }
+    if (!body.email){
+        res.json({
+            error: "email is required"
+        })
+        return
+    }
+
+    const obj = {
+        id: users[users.length-1].id +1 || 1,
+        nombre: body.nombre,
+        email: body.email
+
+    }
+    users.push(obj);
+    res.json(users);
+})
+
+//Modificar Usuario
+userRoutes.put('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const body = req.body
+    const index = users.findIndex(user=> user.id === id)
+    
+    if (index < 0) {
+        res.json({
+            error: "Usuario no encontrado"
+        })
+        return;
+    }
+    if (!body.nombre){
+        res.json({
+            error: "nombre es required"
+        })
+        return
+    }
+
+    users[index].nombre = body.nombre
+
+    res.json(users);
+})
+
+//Borrar usuario
+userRoutes.delete('/users/:id', (req, res) => {
+    const id = parent(req.params.id);
+    const index = users.findIndex(users => user.id === id );
+    
+    if (index<0) {
+        res.json({
+            error: "usuario no encontrado"
+        })
+    }
+
+    users.splice(index,1);
+    res.json(users);
+})
