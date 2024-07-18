1aa59738c8e8b00078822f8648515b191a16ba87
Hans Llanos <hans.llanos@gmail.com>
2024-07-18 11:42:57 -0500

    pc10: creacion de proyecto base

---
+++ modulo2/practicas/pc10/routes/index.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+//import { userRoutes } from "./UserRoutes.js";
+
+const API_router = Router();
+
+export const router = (app) => {
+    app.use('/api/v1', API_router);
+
+    //Importa el Rourtes de Users
+    //API_router.use('/users', userRoutes);
+
+}
