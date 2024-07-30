8a924e86a56e197be078c31d72d3fd8557a26a84
Hans Llanos <hans.llanos@gmail.com>
2024-07-30 15:39:10 -0500

    m3-dia1: clase de la mañama

--- modulo3/dia1/my-first-proyect/src/App.jsx
+++ modulo3/dia1/my-first-proyect/src/App.jsx
@@ -2,7 +2,41 @@ function App() {
 
   return (
     <>
-      <h1>Hola mundo</h1>
+      <nav className="main-nav">
+          <figure className="company-logo">
+            <img src="/img/logo-2.svg" alt="Logo de la empresa" />
+          </figure>
+          <ul className="nav-option">
+            <li href="#" className="nav-item">Home</li>
+            <li href="#" className="nav-item">Service</li>
+            <li href="#" className="nav-item">Contact</li>
+            <li href="#" className="nav-item">About</li>
+          </ul>
+      </nav>
+      <main>
+        <ul className="product-list">
+          <li>
+            <figure>
+              <img src="https://picsum.photos/200/300?random=1"/>
+            </figure>
+          </li>
+          <li>
+            <figure>
+              <img src="https://picsum.photos/200/300?random=2"/>
+            </figure>
+          </li>
+          <li>
+            <figure>
+              <img src="https://picsum.photos/200/300?random=3"/>
+            </figure>
+          </li>
+          <li>
+            <figure>
+              <img src="https://picsum.photos/200/300?random=4"/>
+            </figure>
+          </li>
+        </ul>
+      </main>
     </>
   )
 }
