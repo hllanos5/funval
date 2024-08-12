d57ca164858741638a50f6f49e52fb44460db3c2
Hans Llanos <hans.llanos@gmail.com>
2024-08-12 14:34:10 -0500

    dia-11: integracion con taildwind

---
+++ modulo3/dia11/my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-'>
+      <div>
+        <h2>Hola mundo</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
