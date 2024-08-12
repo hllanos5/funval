9821e4fe22b159987ce19cde10207f95f8d27d67
Hans Llanos <hans.llanos@gmail.com>
2024-08-11 23:00:13 -0500

    mp: implementacion de filtro de adultos y niños

--- modulo3/miniproyecto/miniproyecto/src/App.jsx
+++ modulo3/miniproyecto/miniproyecto/src/App.jsx
@@ -63,6 +63,26 @@ function App() {
               <span>GUESTS</span>
               <input type="text" placeholder='Add guests' disabled/>
             </div>
+            <div className='panel-guest-details'>
+              <div className='detail'>
+                <span>Adults</span>
+                <label>Ages 13 or Above</label>
+                <div className='panel-botones'>
+                  <div className='signo'>-</div>
+                  <div className='contador'>0</div>
+                  <div className='signo'>+</div>
+                </div>
+              </div>
+              <div className='detail'>
+                <span>Children</span>
+                <label>Ages 2 - 12</label>
+                <div className='panel-botones'>
+                  <div className='signo'>-</div>
+                  <div className='contador'>0</div>
+                  <div className='signo'>+</div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='panel-buscar'>
             <button>
