b51968acd8c3c3e5cd8045f385690a5b53433213
Hans Llanos <hans.llanos@gmail.com>
2024-08-11 21:20:19 -0500

    restructuracion de proyecto modo ddd con componentes

--- modulo3/miniproyecto/miniproyecto/src/App.jsx
+++ modulo3/miniproyecto/miniproyecto/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-
+import { List } from './domains/department/page/List';
+import { Nav } from './domains/shared/components/Nav';
 
 function App() {
 
   const[data, setData] = useState([]);
-
   
   async function getData() {
 
@@ -20,61 +20,8 @@ function App() {
 
   return (
     <>
-      <nav>
-        <div className='panel-busqueda'>
-          <div>Helsinki, Findland</div>
-          <div className='buscador'>
-            <input type="text" placeholder='Add guests'/>
-          </div>
-          <div className='buscar'>
-          <span className="material-symbols-outlined">
-            search
-          </span>
-          </div>
-        </div>
-      </nav>
-      <div className='panel-titulo'>
-        <div className='titulo'>
-          <h2>Stay in Filand</h2>
-        </div>
-        <div className='contador'>
-          12+ stays
-        </div>
-      </div>
-      <div className='contenido'>
-        <ul className="card">
-          {
-            data.slice(0,6).map((obj,index) =>
-              <li key={index}>
-                <figure>
-                  <img src={obj.photo}/>
-                </figure>
-                <div className='linea-1'>
-                  {obj.superHost === true && 
-                    <div className='super-host'>SUPER HOST</div>
-                  }
-                  <div className='description'>
-                    {obj.type} 
-                    {obj.beds!== null && <span>. {obj.beds} beds</span>}
-                  </div>
-                  <div className='clasificacion'>
-                    <span className="material-symbols-outlined">
-                    star
-                    </span>{obj.rating}
-                  </div>
-                </div>
-                <div className='linea-2'>
-                  <h2>
-                    {obj.title}
-                  </h2>
-                </div>
-              </li>
-            )
-          }
-            
-        </ul>
-
-      </div>
+      <Nav/>
+      <List data={data}/>
     </>
   )
 }
---
+++ modulo3/miniproyecto/miniproyecto/src/domains/shared/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export function Nav() {
+  return (
+    <nav>
+        <div className='panel-busqueda'>
+            <div>Helsinki, Findland</div>
+            <div className='buscador'>
+            <input type="text" placeholder='Add guests'/>
+            </div>
+            <div className='buscar'>
+            <span className="material-symbols-outlined">
+            search
+            </span>
+            </div>
+        </div>
+    </nav>
+  )
+}
