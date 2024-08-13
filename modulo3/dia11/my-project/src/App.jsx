f70de4848f8ded96702834c59a4e141c06c4be6d
Hans Llanos <hans.llanos@gmail.com>
2024-08-13 14:39:55 -0500

    m3-dia11: sesion por la tarde

--- modulo3/dia11/my-project/src/App.jsx
+++ modulo3/dia11/my-project/src/App.jsx
@@ -4,10 +4,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-'>
-      <div>
-        <h2>Hola mundo</h2>
-      </div>
+    <div className='bg-sky-600 size-96 flex flex-col justify-center items-center grap-8 '>
+      
+      <button className='hover:bg-slate-400 hover:text-blue-700 hover:w-80 active:bg-slate-700'>
+        Hola Mundo
+      </button>
+      <input type="text" name="" id="" className='focus:bg-blue-600/10 focus:ring-2 ring-red-500'/>
     </div>
   )
 }
