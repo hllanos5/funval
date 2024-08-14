0eb97b537d7cade86315cd62198adb56fe1279bf
Hans Llanos <hans.llanos@gmail.com>
2024-08-14 13:12:41 -0500

    m3-dia13: aprendiendo resposive con tailwind

--- modulo3/dia12/my-project/src/App.jsx
+++ modulo3/dia12/my-project/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 
 function App() {
-  const [data, setData] = useState([]);
+  /*const [data, setData] = useState([]);
 
   async function getData() {
     const rs = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink')
@@ -12,23 +12,17 @@ function App() {
 
   useEffect(() => {
     getData()
-  }, [])
+  }, []) */
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <div className='grid grid-cols-4 m-4 gap-4'>
-      {
-        data.map((obj, item)=> 
-          <div className='border-2 border-slate-500 rounded flex flex-col p-6 content-center items-center'>
-            <div><img src={obj.strDrinkThumb}/> </div>
-            <div>{obj.strDrink}</div>
-          </div>
-        )
-      }      
-    </div>
-    </>
+    <main className=''>
+      <header className='w-full h-[60px] border-solid border-2 border-indigo-600'>Header</header>
+      <div className='flex flex-wrap h-[calc(100vh-120px)] '>
+        <div className='border-solid border-2 border-indigo-100  grid h-1/3 w-full md:h-1/2 lg:w-1/3 lg:h-full lg:order-2'>Main</div>
+        <div className='border-solid border-2 border-indigo-200 h-1/3 w-full md:w-1/2  md:h-1/2 lg:w-1/3 lg:h-full lg:order-1' >Left</div>
+        <div className='border-solid border-2 border-indigo-300 h-1/3 w-full md:w-1/2  md:h-1/2 lg:w-1/3 lg:h-full lg:order-3'>Right</div>
+      </div>
+        <footer className='w-full h-[60px] border-solid border-2 border-indigo-600'>Footer</footer>
+    </main>
   )
 }
 
