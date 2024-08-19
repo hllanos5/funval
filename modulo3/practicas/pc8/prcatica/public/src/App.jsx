ec3d5861344a5d5dc05cc8faaceba8e69f761a9b
Hans Llanos <hans.llanos@gmail.com>
2024-08-19 18:18:21 -0500

    m3-pc8: se agrega consumo de primer api con fetch

--- modulo3/practicas/pc8/prcatica/public/src/App.jsx
+++ modulo3/practicas/pc8/prcatica/public/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = `https://cataas.com/fact`;
+const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
 
 export function App() {
-  return (
-    <div>
-      <h1>App de gatitos</h1>
-    </div>
-  )
+
+    const [fact, setFact] = useState('ddds');
+    
+    useEffect(()=>{
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => setFact(data.fact))
+
+    }, [])
+    
+    return (
+        <div>
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>}
+        </div>
+    )
 }
