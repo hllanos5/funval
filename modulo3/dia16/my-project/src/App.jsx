e74666bec6af05daed70d6d1fa0ed8f9c64213c1
Hans Llanos <hans.llanos@gmail.com>
2024-08-19 14:25:25 -0500

    m3-dia16: mejora de avaluar ganar de 3x

--- modulo3/dia16/my-project/src/App.jsx
+++ modulo3/dia16/my-project/src/App.jsx
@@ -17,15 +17,16 @@ const {game, handleMove, turn} = useGame();
               className={`size-28 bg-slate-800 grid place-content-center mx-auto text-white ${!item.player && 'cursor-pointer'}`}
               onClick={()=>handleMove(item.id)}>
                 {item.player === 1 && 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z" />
+                  <svg key= {item}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z" />
                   </svg>
                 }
                 {item.player === 2 && 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                  </svg>                
+                  <svg key= {item}  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                  </svg>
+                
                 }
               </div>
             )
--- modulo3/dia16/my-project/src/hooks/useGame.jsx
+++ modulo3/dia16/my-project/src/hooks/useGame.jsx
@@ -3,15 +3,55 @@ import { board } from '../utils/constants'
 
 export function useGame() {
     const [game, setGame] = useState(board)
-    const [turn, setTurn] = useState(Math.floor(Math.random() * (2 - 1 + 1) + 1))
+    const [turn, setTurn] = useState(Math.floor(Math.random() * (2 - 1 + 1) + 1));
+    const [numJugada, setNumJugada] = useState(0);
+
+    const [jugadaPlayer1, setJugadaPlayer1] = useState([
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ]);
+    const [jugadaPlayer2, setJugadaPlayer2] = useState([
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+    ]);
+    const [winner, setWinner] = useState([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],//Diagonal
+        [2, 4, 6],//Diagonal
+      ]);
 
     const handleMove= (id) => {
         const newBoard = [...game];
+        setNumJugada(numJugada+1);
 
         if(!newBoard[id].player){
             newBoard[id].player = turn;
             setGame (newBoard);
             changeTurn()
+
+           if(turn === 1) { //Jugador 1
+            guardarJugaba(jugadaPlayer1, setJugadaPlayer1, id);
+            
+            if(evaluarGanador(jugadaPlayer1)){
+                console.log("jugador 1 winner");
+            }
+           }
+           if(turn === 2){  //jugador 2
+            guardarJugaba(jugadaPlayer2, setJugadaPlayer2, id)
+            if(evaluarGanador(jugadaPlayer2)){
+                console.log("jugador 2 winner");
+            }
+           }
+           console.log("Jugador1: ",jugadaPlayer1);
+           console.log("Jugador2: ",jugadaPlayer2);
+           
         }
         
     }
@@ -19,6 +59,32 @@ export function useGame() {
     function changeTurn (){
         const newTurn = turn === 1 ? 2: 1 ;
         setTurn(newTurn);
+    }
+
+    function guardarJugaba(jugador, handleSet,  posicion){
+        console.log(posicion);
+        if(posicion<=2){
+            handleSet([...jugador, jugador[0][posicion] = posicion]);
+        }
+        else if(posicion<=5){
+            handleSet([...jugador, jugador[1][posicion-3] = posicion]);
+        }
+        else{
+            handleSet([...jugador, jugador[2][posicion-6] = posicion]);
+        }
+    }
+
+    function evaluarGanador(jugador){
+        
+        console.log(winner);
+        winner.forEach((obj1, index1) => {
+            jugador.forEach((obj2, index2) =>{
+                if(obj1 === obj2){
+                    return true;
+                }
+            })
+        });
+        
     }
 
     return {
