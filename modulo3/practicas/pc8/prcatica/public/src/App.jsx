e47ebd3c875ed2a807dee7bd5d7f0c0ec724b040
Hans Llanos <hans.llanos@gmail.com>
2024-08-19 19:16:30 -0500

    m3-pc8: separando en 2 effect

--- modulo3/practicas/pc8/prcatica/public/src/App.jsx
+++ modulo3/practicas/pc8/prcatica/public/src/App.jsx
@@ -17,18 +17,24 @@ export function App() {
             const { fact } = data;
             setFact(fact);
             
-            const firstWord = fact.split(' ',3).join(' ');
-            console.log(firstWord);
-            fetch (`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
-            .then(res => res.json())
-            .then(response => {
-                const { _id } = response
-               const url = `/cat/${_id}/says/${firstWord}`                
-                setImageUrl(`https://cataas.com/${url}`);
-            })
         })
 
     }, [])
+
+    useEffect(()=> {
+        if(!fact) return
+        
+        const firstWord = fact.split(' ',3).join(' ');
+            
+        fetch (`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+        .then(res => res.json())
+        .then(response => {
+            const { _id } = response
+            const url = `/cat/${_id}/says/${firstWord}`                
+            setImageUrl(`https://cataas.com/${url}`);
+        })
+
+    },[fact])
     
     return (
         <main>
