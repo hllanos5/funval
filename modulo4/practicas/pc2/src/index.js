3c64af70c48fb60394d9d0b581852f7f6a61b6b1
Hans Llanos <hans.llanos@gmail.com>
2024-09-03 18:33:52 -0500

    m4-pc2: creacion de logica para generar token

--- modulo4/practicas/pc2/src/index.js
+++ modulo4/practicas/pc2/src/index.js
@@ -1,9 +1,33 @@
 import express from 'express';
+import { generateToken, verifyToken } from './auth.js';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido a mi aplicación Express con JWT!');
+app.use(express.json());
+
+app.post('/login', (req, res) => {
+  // Aquí verificarías las credenciales del usuario, por simplicidad, omitiremos este paso
+  const { username, password } = req.body;
+
+  // Supongamos que la autenticación es exitosa
+  const token = generateToken({ username });
+
+  res.json({ token });
+});
+
+app.get('/protected', (req, res) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  try {
+    const decoded = verifyToken(token.split(' ')[1]);
+    res.json({ message: `Bienvenido, ${decoded.username}!` });
+  } catch (error) {
+    res.status(401).json({ error: 'Token inválido' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
