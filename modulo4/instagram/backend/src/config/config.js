6ef34593ba9a0413de57071e5b5507d96d4ebbc4
Hans Llanos <hans.llanos@gmail.com>
2024-09-12 14:21:49 -0500

    m4-dia6: se agrega rutas de imagenes

--- modulo4/instagram/backend/src/config/config.js
+++ modulo4/instagram/backend/src/config/config.js
@@ -9,4 +9,4 @@ export const DB_PORT = process.env.DB_PORT || 3306
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_DATABASE = process.env.DB_DATABASE || 'instagram'
 export const SECRET_KEY = process.env.SECRET_KEY
-export const allowedOrigins = ['http://localhost:5173']
+export const allowedOrigins = [undefined, 'http://localhost:5173']
--- modulo4/instagram/backend/src/index.js
+++ modulo4/instagram/backend/src/index.js
@@ -3,6 +3,7 @@ import { PORT } from './config/config.js'
 import morgan from 'morgan'
 import userRoutes from './routes/users.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import imagesRoutes from './routes/images.routes.js'
 import { validateCORS } from './middlewares/middleware.js'
 
 const app = express()
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use(validateCORS)
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/images', imagesRoutes)
 
 app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`))
